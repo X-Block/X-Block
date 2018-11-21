a002715bec45a182c55a66d7523ac72b9664254c
MemoryIX <admin@x-block.io>
2018-11-21 23:24:10 +0800

    Update stackitem.go

--- vm/types/stackitem.go
+++ vm/types/stackitem.go
@@ -5,5 +5,10 @@ import (
 )
 
 type StackItem interface {
-
+	Equals(other StackItem) bool
+	GetBigInteger() *big.Int
+	GetBoolean() bool
+	GetByteArray() []byte
+	GetInterface()
+	GetArray() []StackItem
 }
