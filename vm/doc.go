e88428186ac651612ffb2930acd47e59e2113d05
Pepppo <bounty@x-block.io>
2018-11-21 13:54:03 +0800

    Update doc.go

--- vm/doc.go
+++ vm/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The XBlock Dev team
+Copyright 2018 The X-Block Dev team
 
 The go-vm library is free software: you can redistribute it and/or modify
 it under the terms of the APACHA License
