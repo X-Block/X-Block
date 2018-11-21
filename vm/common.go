3d3ca1af839471e59021da3e1b25533b7143c7fb
moonXBlock <press@x-block.io>
2018-11-21 23:42:31 +0800

    Update common.go

--- vm/common.go
+++ vm/common.go
@@ -17,3 +17,10 @@ func (c BigIntSorter) Swap(i, j int) {
 	if i >= 0 && i < len(c) && j >= 0 && j < len(c) { 		c[i], c[j] = c[j], c[i]
 	}
 }
+func (c BigIntSorter) Less(i, j int) bool {
+	if i >= 0 && i < len(c) && j >= 0 && j < len(c) { 		return c[i].Cmp(&c[j]) < 0
+	}
+
+	return false
+}
+
