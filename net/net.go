840640b079deda5510f6210fefd08af5d73c6ba8
MemoryIX <admin@x-block.io>
2018-11-22 09:56:06 +0800

    Update net.go

--- net/net.go
+++ net/net.go
@@ -10,3 +10,14 @@ import (
 	"XBlock/net/protocol"
 )
 
+type Neter interface {
+	GetTxnPool(cleanPool bool) map[Uint256]*transaction.Transaction
+	SynchronizeTxnPool()
+	Xmit(interface{}) error
+	GetEvent(eventName string) *events.Event
+	GetBookKeepersAddrs() ([]*crypto.PubKey, uint64)
+	CleanSubmittedTransactions(block *ledger.Block) error
+	GetNeighborNoder() []protocol.Noder
+	Tx(buf []byte)
+}
+
