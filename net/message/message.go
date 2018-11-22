cdf10456001f2eba9c6fc4c96a00391f6f30de8d
Pepppo <bounty@x-block.io>
2018-11-22 21:59:06 +0800

    Update message.go

--- net/message/message.go
+++ net/message/message.go
@@ -11,3 +11,44 @@ import (
 	"fmt"
 )
 
+type Messager interface {
+	Verify([]byte) error
+	Serialization() ([]byte, error)
+	Deserialization([]byte) error
+	Handle(Noder) error
+}
+
+type msgHdr struct {
+	Magic uint32
+	
+	CMD      [MSGCMDLEN]byte 
+	Length   uint32
+	Checksum [CHECKSUMLEN]byte
+}
+
+
+type msgCont struct {
+	hdr msgHdr
+	p   interface{}
+}
+
+type varStr struct {
+	len uint
+	buf []byte
+}
+
+type filteradd struct {
+	msgHdr
+	
+}
+
+type filterclear struct {
+	msgHdr
+	
+}
+
+type filterload struct {
+	msgHdr
+	
+}
+
