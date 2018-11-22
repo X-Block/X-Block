246ae90db74746e2337ed210cf64b4f3f9ea888d
Pepppo <bounty@x-block.io>
2018-11-22 22:24:06 +0800

    Update transaction.go

--- net/message/transaction.go
+++ net/message/transaction.go
@@ -25,3 +25,23 @@ type trn struct {
 	txn transaction.Transaction
 }
 
+func (msg trn) Handle(node Noder) error {
+	log.Debug()
+	log.Debug("RX Transaction message")
+	tx := &msg.txn
+	if !node.LocalNode().ExistedID(tx.Hash()) {
+		if err := va.VerifyTransaction(tx); err != nil {
+			return errors.New("[VerifyTransaction] error")
+		}
+		if err := va.VerifyTransactionWithLedger(tx, ledger.DefaultLedger); err != nil {
+			return errors.New("[VerifyTransactionWithLedger] error")
+		}
+		node.LocalNode().AppendTxnPool(&(msg.txn))
+		node.LocalNode().IncRxTxnCnt()
+		log.Debug("RX Transaction message hash", msg.txn.Hash())
+		log.Debug("RX Transaction message type", msg.txn.TxType)
+	}
+
+	return nil
+}
+
