0c0b3291546076b91fa1fd2736cda16abcd56e43
MemoryIX <admin@x-block.io>
2018-11-22 21:45:28 +0800

    Create inventory.go

---
+++ net/message/inventory.go
@@ -0,0 +1,16 @@
+package message
+
+import (
+	. "XBlock/common"
+	"XBlock/common/log"
+	"XBlock/common/serialization"
+	"XBlock/core/ledger"
+	. "XBlock/net/protocol"
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
