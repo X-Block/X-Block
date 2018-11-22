bc1c7f22c08050f63227cfcbe066d5b235b7e065
Skyycoding <zach@x-block.io>
2018-11-22 09:17:45 +0800

    Update eventTest.go

--- events/eventTest.go
+++ events/eventTest.go
@@ -5,3 +5,16 @@ import (
 	"fmt"
 )
 
+func TestNewEvent(t *testing.T) {
+	event := NewEvent()
+
+	var subscriber1 EventFunc = func(v interface{}){
+		fmt.Println("subscriber1 event func.")
+	}
+
+	var subscriber2 EventFunc = func(v interface{}){
+		fmt.Println("subscriber2 event func.")
+	}
+
+
+}
