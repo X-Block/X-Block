08646682d4f5d562dbdc33a821d8c5576931560c
DicyLa <robert@x-block.io>
2018-11-22 22:29:40 +0800

    Create pong.go

---
+++ net/message/pong.go
@@ -0,0 +1,12 @@
+package message
+
+import (
+	"XBlock/common/log"
+	"XBlock/common/serialization"
+	"XBlock/core/ledger"
+	. "XBlock/net/protocol"
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+)
+
