fe2c7c8a71555796f0c587c37f2cba821ee744ad
DocXBlock <info@x-block.io>
2018-11-22 09:39:53 +0800

    Update errorsTest.go

--- errors/errorsTest.go
+++ errors/errorsTest.go
@@ -12,3 +12,17 @@ var (
 
 
 
+func TestNewDetailErr(t *testing.T) {
+	e := NewDetailErr(TestRootError,ErrUnknown,"Test New Detail Error")
+	if e == nil {
+		t.Fatal("NewDetailErr should not return nil.")
+	}
+	fmt.Println(e.Error())
+
+	msg := CallStacksString(GetCallStacks(e))
+
+	fmt.Println(msg)
+
+	
+}
+
