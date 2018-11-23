6cfce4e3c0158c18c5c5a4aafb996b48ea5eac6a
Skyycoding <zach@x-block.io>
2018-11-23 15:10:20 +0800

    Update common.go

--- net/httpjson/common.go
+++ net/httpjson/common.go
@@ -242,3 +242,29 @@ func responsePacking(result interface{}) map[string]interface{} {
 }
 
 
+func Call(address string, method string, id interface{}, params []interface{}) ([]byte, error) {
+	data, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"id":     id,
+		"params": params,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Marshal JSON request: %v\n", err)
+		return nil, err
+	}
+	resp, err := http.Post(address, "application/json", strings.NewReader(string(data)))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "POST request: %v\n", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "GET response: %v\n", err)
+		return nil, err
+	}
+
+	return body, nil
+}
+
