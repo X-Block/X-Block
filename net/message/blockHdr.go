47010e2dc1d33803eec0d7a1820096ee802a5b8b
DicyLa <robert@x-block.io>
2018-11-23 14:53:03 +0800

    Create blockHdr.go

---
+++ net/message/blockHdr.go
@@ -0,0 +1,14 @@
+package message
+
+import (
+	"XBlock/common"
+	"XBlock/common/log"
+	"XBlock/common/serialization"
+	"XBlock/core/ledger"
+	. "XBlock/net/protocol"
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"errors"
+)
+
