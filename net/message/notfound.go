5672a2937d9f2d3d6cd006c617fe93dc9592b007
DicyLa <robert@x-block.io>
2018-11-23 14:24:28 +0800

    Update notfound.go

--- net/message/notfound.go
+++ net/message/notfound.go
@@ -15,3 +15,35 @@ type notFound struct {
 	hash common.Uint256
 }
 
+func NewNotFound(hash common.Uint256) ([]byte, error) {
+	log.Debug()
+	var msg notFound
+	msg.hash = hash
+	msg.msgHdr.Magic = NETMAGIC
+	cmd := "notfound"
+	copy(msg.msgHdr.CMD[0:len(cmd)], cmd)
+	tmpBuffer := bytes.NewBuffer([]byte{})
+	msg.hash.Serialize(tmpBuffer)
+	p := new(bytes.Buffer)
+	err := binary.Write(p, binary.LittleEndian, tmpBuffer.Bytes())
+	if err != nil {
+		log.Error("Binary Write failed at new notfound Msg")
+		return nil, err
+	}
+	s := sha256.Sum256(p.Bytes())
+	s2 := s[:]
+	s = sha256.Sum256(s2)
+	buf := bytes.NewBuffer(s[:4])
+	binary.Read(buf, binary.LittleEndian, &(msg.msgHdr.Checksum))
+	msg.msgHdr.Length = uint32(len(p.Bytes()))
+	log.Debug("The message payload length is ", msg.msgHdr.Length)
+
+	m, err := msg.Serialization()
+	if err != nil {
+		log.Error("Error Convert net message ", err.Error())
+		return nil, err
+	}
+
+	return m, nil
+}
+
