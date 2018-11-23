f86d65f21f89640b5ad03bdadd697f62eab7cd65
MemoryIX <admin@x-block.io>
2018-11-23 15:37:02 +0800

    Update common.go

--- net/httprestful/common/common.go
+++ net/httprestful/common/common.go
@@ -45,3 +45,41 @@ func GetConnectionCount(cmd map[string]interface{}) map[string]interface{} {
 }
 
 
+func GetBlockHeight(cmd map[string]interface{}) map[string]interface{} {
+	resp := ResponsePack(Err.SUCCESS)
+	resp["Result"] = ledger.DefaultLedger.Blockchain.BlockHeight
+	return resp
+}
+func getBlock(hash Uint256) (interface{}, int64) {
+	block, err := ledger.DefaultLedger.Store.GetBlock(hash)
+	if err != nil {
+		return "", Err.UNKNOWN_BLOCK
+	}
+
+	blockHead := &BlockHead{
+		Version:          block.Blockdata.Version,
+		PrevBlockHash:    ToHexString(block.Blockdata.PrevBlockHash.ToArray()),
+		TransactionsRoot: ToHexString(block.Blockdata.TransactionsRoot.ToArray()),
+		Timestamp:        block.Blockdata.Timestamp,
+		Height:           block.Blockdata.Height,
+		ConsensusData:    block.Blockdata.ConsensusData,
+		NextBookKeeper:   ToHexString(block.Blockdata.NextBookKeeper.ToArray()),
+		Program: ProgramInfo{
+			Code:      ToHexString(block.Blockdata.Program.Code),
+			Parameter: ToHexString(block.Blockdata.Program.Parameter),
+		},
+		Hash: ToHexString(hash.ToArray()),
+	}
+
+	trans := make([]*Transactions, len(block.Transactions))
+	for i := 0; i < len(block.Transactions); i++ {
+		trans[i] = TransArryByteToHexString(block.Transactions[i])
+	}
+
+	b := BlockInfo{
+		Hash:         ToHexString(hash.ToArray()),
+		BlockData:    blockHead,
+		Transactions: trans,
+	}
+	return b, Err.SUCCESS
+}
