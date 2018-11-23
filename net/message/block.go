6baee1da4e0485882b0289391c1ccec7e7a3aeb9
DicyLa <robert@x-block.io>
2018-11-23 14:38:33 +0800

    Update block.go

--- net/message/block.go
+++ net/message/block.go
@@ -21,3 +21,19 @@ type block struct {
 	blk ledger.Block
 }
 
+func (msg block) Handle(node Noder) error {
+	log.Debug("RX block message")
+	hash := msg.blk.Hash()
+	if ledger.DefaultLedger.BlockInLedger(hash) {
+		log.Warn("Receive duplicated block: ", hash)
+		return errors.New("Received duplicate block")
+	}
+	if err := ledger.DefaultLedger.Blockchain.AddBlock(&msg.blk); err != nil {
+		log.Error("Block adding error: ", hash)
+		return err
+	}
+	node.RemoveFlightHeight(msg.blk.Blockdata.Height)
+	node.LocalNode().GetEvent("block").Notify(events.EventNewInventory, &msg.blk)
+	return nil
+}
+
