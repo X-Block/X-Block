2c08e74cef2ec8a424375b8ad3e46f7cd4f5192a
MemoryIX <admin@x-block.io>
2018-11-23 15:39:51 +0800

    Update common.go

--- net/httprestful/common/common.go
+++ net/httprestful/common/common.go
@@ -273,3 +273,55 @@ func SetNoticeServerAddr(cmd map[string]interface{}) map[string]interface{} {
 	resp["Result"] = Parameters.NoticeServerAddr
 	return resp
 }
+func ResponsePack(errCode int64) map[string]interface{} {
+	resp := map[string]interface{}{
+		"Action":  "",
+		"Result":  "",
+		"Error":   errCode,
+		"Desc":    "",
+		"Version": "1.0.0",
+	}
+	return resp
+}
+
+func PostRequest(cmd map[string]interface{}, addr string) (map[string]interface{}, error) {
+
+	var repMsg = make(map[string]interface{})
+
+	data, err := json.Marshal(cmd)
+	if err != nil {
+		return repMsg, err
+	}
+	reqData := bytes.NewBuffer(data)
+	transport := http.Transport{
+		Dial: func(netw, addr string) (net.Conn, error) {
+			conn, err := net.DialTimeout(netw, addr, time.Second*2)
+			if err != nil {
+				return nil, err
+			}
+			conn.SetDeadline(time.Now().Add(time.Second * 5))
+			return conn, nil
+		},
+		DisableKeepAlives: true,
+	}
+	client := &http.Client{Transport: &transport}
+	request, err := http.NewRequest("POST", addr, reqData)
+	if err != nil {
+		return repMsg, err
+	}
+	request.Header.Set("Content-type", "application/json")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return repMsg, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 {
+		body, _ := ioutil.ReadAll(response.Body)
+		if err := json.Unmarshal(body, &repMsg); err == nil {
+			return repMsg, err
+		}
+	}
+	return repMsg, err
+}
