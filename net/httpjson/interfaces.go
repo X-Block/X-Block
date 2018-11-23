02545cbe5805779ec559772ff600f83d793ffa03
Skyycoding <zach@x-block.io>
2018-11-23 15:21:17 +0800

    Update interfaces.go

--- net/httpjson/interfaces.go
+++ net/httpjson/interfaces.go
@@ -18,3 +18,88 @@ const (
 	RANDBYTELEN = 4
 )
 
+func TransArryByteToHexString(ptx *tx.Transaction) *Transactions {
+
+	trans := new(Transactions)
+	trans.TxType = ptx.TxType
+	trans.PayloadVersion = ptx.PayloadVersion
+	trans.Payload = TransPayloadToHex(ptx.Payload)
+	trans.Nonce = ptx.Nonce
+
+	n := 0
+	trans.Attributes = make([]TxAttributeInfo, len(ptx.Attributes))
+	for _, v := range ptx.Attributes {
+		trans.Attributes[n].Usage = v.Usage
+		trans.Attributes[n].Data = ToHexString(v.Data)
+		n++
+	}
+
+	n = 0
+	trans.UTXOInputs = make([]UTXOTxInputInfo, len(ptx.UTXOInputs))
+	for _, v := range ptx.UTXOInputs {
+		trans.UTXOInputs[n].ReferTxID = ToHexString(v.ReferTxID.ToArray())
+		trans.UTXOInputs[n].ReferTxOutputIndex = v.ReferTxOutputIndex
+		n++
+	}
+
+	n = 0
+	trans.BalanceInputs = make([]BalanceTxInputInfo, len(ptx.BalanceInputs))
+	for _, v := range ptx.BalanceInputs {
+		trans.BalanceInputs[n].AssetID = ToHexString(v.AssetID.ToArray())
+		trans.BalanceInputs[n].Value = v.Value
+		trans.BalanceInputs[n].ProgramHash = ToHexString(v.ProgramHash.ToArray())
+		n++
+	}
+
+	n = 0
+	trans.Outputs = make([]TxoutputInfo, len(ptx.Outputs))
+	for _, v := range ptx.Outputs {
+		trans.Outputs[n].AssetID = ToHexString(v.AssetID.ToArray())
+		trans.Outputs[n].Value = v.Value
+		trans.Outputs[n].ProgramHash = ToHexString(v.ProgramHash.ToArray())
+		n++
+	}
+
+	n = 0
+	trans.Programs = make([]ProgramInfo, len(ptx.Programs))
+	for _, v := range ptx.Programs {
+		trans.Programs[n].Code = ToHexString(v.Code)
+		trans.Programs[n].Parameter = ToHexString(v.Parameter)
+		n++
+	}
+
+	n = 0
+	trans.AssetOutputs = make([]TxoutputMap, len(ptx.AssetOutputs))
+	for k, v := range ptx.AssetOutputs {
+		trans.AssetOutputs[n].Key = k
+		trans.AssetOutputs[n].Txout = make([]TxoutputInfo, len(v))
+		for m := 0; m < len(v); m++ {
+			trans.AssetOutputs[n].Txout[m].AssetID = ToHexString(v[m].AssetID.ToArray())
+			trans.AssetOutputs[n].Txout[m].Value = v[m].Value
+			trans.AssetOutputs[n].Txout[m].ProgramHash = ToHexString(v[m].ProgramHash.ToArray())
+		}
+		n += 1
+	}
+
+	n = 0
+	trans.AssetInputAmount = make([]AmountMap, len(ptx.AssetInputAmount))
+	for k, v := range ptx.AssetInputAmount {
+		trans.AssetInputAmount[n].Key = k
+		trans.AssetInputAmount[n].Value = v
+		n += 1
+	}
+
+	n = 0
+	trans.AssetOutputAmount = make([]AmountMap, len(ptx.AssetOutputAmount))
+	for k, v := range ptx.AssetOutputAmount {
+		trans.AssetInputAmount[n].Key = k
+		trans.AssetInputAmount[n].Value = v
+		n += 1
+	}
+
+	mhash := ptx.Hash()
+	trans.Hash = ToHexString(mhash.ToArray())
+
+	return trans
+}
+
