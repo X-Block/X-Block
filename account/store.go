d2e2f6907645d770ae08668b9fd0951d863eea5f
DocXBlock <info@x-block.io>
2018-11-24 10:12:44 +0800

    Update store.go

--- account/store.go
+++ account/store.go
@@ -5,3 +5,14 @@ import (
 	ct "XBlock/core/contract"
 )
 
+type IClientStore interface {
+	BuildDatabase(path string)
+
+	SaveStoredData(name string, value []byte)
+
+	LoadStoredData(name string) []byte
+
+	LoadAccount() map[Uint160]*Account
+
+	LoadContracts() map[Uint160]*ct.Contract
+}
