56085f6bcf5ebb67f878b416d7fe6aad7f7246fc
DocXBlock <info@x-block.io>
2018-11-24 09:50:59 +0800

    Update infoUpdate.go

--- net/node/infoUpdate.go
+++ net/node/infoUpdate.go
@@ -33,3 +33,27 @@ func (node *node) GetBlkHdrs() {
 	}
 }
 
+func (node *node) SyncBlk() {
+	headerHeight := ledger.DefaultLedger.Store.GetHeaderHeight()
+	currentBlkHeight := ledger.DefaultLedger.Blockchain.BlockHeight
+	if currentBlkHeight >= headerHeight {
+		return
+	}
+	var dValue int32
+	var reqCnt uint32
+	var i uint32
+	noders := node.local.GetNeighborNoder()
+	for _, n := range noders {
+		n.RemoveFlightHeightLessThan(currentBlkHeight)
+		count := MAXREQBLKONCE - uint32(n.GetFlightHeightCnt())
+		dValue = int32(headerHeight - currentBlkHeight - reqCnt)
+		for i = 1; i <= count && dValue >= 0; i++ {
+			hash := ledger.DefaultLedger.Store.GetHeaderHashByHeight(currentBlkHeight + reqCnt)
+			ReqBlkData(n, hash)
+			n.StoreFlightHeight(currentBlkHeight + reqCnt)
+			reqCnt++
+			dValue--
+		}
+	}
+}
+
