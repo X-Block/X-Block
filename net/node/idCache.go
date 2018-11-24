90e38b9c10b3b3575f255c5611f2600e05d33c07
DocXBlock <info@x-block.io>
2018-11-24 09:59:24 +0800

    Update idCache.go

--- net/node/idCache.go
+++ net/node/idCache.go
@@ -10,3 +10,15 @@ type idCache struct {
 	list map[common.Uint256]bool
 }
 
+func (c *idCache) init() {
+}
+
+func (c *idCache) add() {
+}
+
+func (c *idCache) del() {
+}
+
+func (c *idCache) ExistedID(id common.Uint256) bool {
+	return false
+}
