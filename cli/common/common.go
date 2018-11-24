511f083af82629c7b31b98975f8073d0915450bd
DocXBlock <info@x-block.io>
2018-11-24 10:43:32 +0800

    Update common.go

--- cli/common/common.go
+++ cli/common/common.go
@@ -46,3 +46,14 @@ func PrintError(c *cli.Context, err error, cmd string) {
 	cli.ShowCommandHelp(c, cmd)
 }
 
+func FormatOutput(o []byte) error {
+	var out bytes.Buffer
+	err := json.Indent(&out, o, "", "\t")
+	if err != nil {
+		return err
+	}
+	out.Write([]byte("\n"))
+	_, err = out.WriteTo(os.Stdout)
+
+	return err
+}
