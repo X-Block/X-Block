e772475eefd970159fe23503517bec277164bcf7
moonXBlock <press@x-block.io>
2018-11-24 09:32:43 +0800

    Create server.go

---
+++ net/httprestful/server.go
@@ -0,0 +1,12 @@
+package httprestful
+
+import (
+	. "XBlock/common/config"
+	"XBlock/core/ledger"
+	"XBlock/events"
+	"XBlock/net/httprestful/common"
+	. "XBlock/net/httprestful/restful"
+	. "XBlock/net/protocol"
+	"strconv"
+)
+
