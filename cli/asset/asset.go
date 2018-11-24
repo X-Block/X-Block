76f49e6dbaeab668d3442b392e9dbf06c39fbfc6
DocXBlock <info@x-block.io>
2018-11-24 10:53:49 +0800

    Create asset.go

---
+++ cli/asset/asset.go
@@ -0,0 +1,23 @@
+package asset
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	. "XBlock/cli/common"
+	"XBlock/account"
+	. "XBlock/common"
+	. "XBlock/core/asset"
+	"XBlock/core/contract"
+	"XBlock/core/signature"
+	"XBlock/core/transaction"
+	"XBlock/net/httpjsonrpc"
+
+	"github.com/urfave/cli"
+)
+
