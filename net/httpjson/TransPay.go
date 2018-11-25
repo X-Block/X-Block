9a1f50b854815440012dba3daf6a68dce891f7da
DicyLa <robert@x-block.io>
2018-11-25 09:34:08 +0800

    Create TransPay.go

---
+++ net/httpjson/TransPay.go
@@ -0,0 +1,11 @@
+package httpjsonrpc
+
+import (
+	. "XBlock/common"
+	"XBlock/core/asset"
+	. "XBlock/core/contract"
+	. "XBlock/core/transaction"
+	"XBlock/core/transaction/payload"
+	"bytes"
+)
+
