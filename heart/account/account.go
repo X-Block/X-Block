f05b84da5e7510e196965a7451b0ee4471a507d7
DicyLa <robert@x-block.io>
2018-11-25 10:00:01 +0800

    Update account.go

--- heart/account/account.go
+++ heart/account/account.go
@@ -21,3 +21,14 @@ func NewAccountState(programHash common.Uint160, balances map[common.Uint256]com
 	return &accountState
 }
 
+func(accountState *AccountState)Serialize(w io.Writer) error {
+	accountState.ProgramHash.Serialize(w)
+	serialization.WriteBool(w, accountState.IsFrozen)
+	serialization.WriteUint64(w, uint64(len(accountState.Balances)))
+	for k, v := range accountState.Balances {
+		k.Serialize(w)
+		v.Serialize(w)
+	}
+	return nil
+}
+
