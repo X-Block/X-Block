1317c493fc140d2655240f6df23f5f84ac5934e1
DicyLa <robert@x-block.io>
2018-11-25 09:29:56 +0800

    Update localServer.go

--- net/httpjson/localServer.go
+++ net/httpjson/localServer.go
@@ -8,3 +8,15 @@ import (
 )
 
 
+func StartLocalServer() {
+	log.Debug()
+	http.HandleFunc(LocalDir, Handle)
+
+	HandleFunc("getneighbor", getNeighbor)
+	HandleFunc("getnodestate", getNodeState)
+	HandleFunc("startconsensus", startConsensus)
+	HandleFunc("stopconsensus", stopConsensus)
+	HandleFunc("sendsampletransaction", sendSampleTransaction)
+	HandleFunc("setdebuginfo", setDebugInfo)
+
+	
