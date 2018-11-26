6bc62dc0dc8bf0cff5cbec0e151b2908a5239762
MemoryIX <admin@x-block.io>
2018-11-26 11:20:05 +0800

    Update store.go

--- heart/store/store.go
+++ heart/store/store.go
@@ -11,3 +11,14 @@ type IIterator interface {
 	Release()
 }
 
+type IStore interface {
+	Put(key []byte, value []byte) error
+	Get(key []byte) ([]byte, error)
+	Delete(key []byte) error
+	NewBatch() error
+	BatchPut(key []byte, value []byte) error
+	BatchDelete(key []byte) error
+	BatchCommit() error
+	Close() error
+	NewIterator(prefix []byte) IIterator
+}
