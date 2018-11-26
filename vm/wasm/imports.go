6ac15391935917806d5909040b02956b98de864b
MemoryIX <admin@x-block.io>
2018-11-26 20:55:37 +0800

    Update imports.go

--- vm/wasm/imports.go
+++ vm/wasm/imports.go
@@ -27,3 +27,40 @@ type FuncImport struct {
 	Type uint32
 }
 
+func (FuncImport) isImport() {}
+func (FuncImport) Kind() External {
+	return ExternalFunction
+}
+func (f FuncImport) MarshalWASM(w io.Writer) error {
+	_, err := leb128.WriteVarUint32(w, uint32(f.Type))
+	return err
+}
+
+type TableImport struct {
+	Type Table
+}
+
+func (TableImport) isImport() {}
+func (TableImport) Kind() External {
+	return ExternalTable
+}
+func (t TableImport) MarshalWASM(w io.Writer) error {
+	return t.Type.MarshalWASM(w)
+}
+
+type MemoryImport struct {
+	Type Memory
+}
+
+func (MemoryImport) isImport() {}
+func (MemoryImport) Kind() External {
+	return ExternalMemory
+}
+func (t MemoryImport) MarshalWASM(w io.Writer) error {
+	return t.Type.MarshalWASM(w)
+}
+
+type GlobalVarImport struct {
+	Type GlobalVar
+}
+
