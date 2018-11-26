614ade79283c085d0c6f749d1c5b31ed2d7393d2
MemoryIX <admin@x-block.io>
2018-11-26 11:14:10 +0800

    Update accountTest.go

--- account/accountTest.go
+++ account/accountTest.go
@@ -8,3 +8,19 @@ import (
 	"testing"
 )
 
+func TestClient(t *testing.T) {
+	t.Log("created client start!")
+	crypto.SetAlg(crypto.P256R1)
+	dir := "./data/"
+	err := os.MkdirAll(dir, 0888)
+	if err != nil {
+		t.Log("create dir ", dir, " error: ", err)
+	} else {
+		t.Log("create dir ", dir, " success!")
+	}
+	for i := 0; i < 10000; i++ {
+		p := path.Join(dir, fmt.Sprintf("wallet%d.txt", i))
+		fmt.Println("client path", p)
+		CreateClient(p, []byte(DefaultPin))
+	}
+}
