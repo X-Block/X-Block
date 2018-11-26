a6ab2dd8c723513af04d706e15c76b6569a19301
Skyycoding <zach@x-block.io>
2018-11-26 13:25:49 +0800

    Update ChainStore.go

--- heart/store/ChainStore/ChainStore.go
+++ heart/store/ChainStore/ChainStore.go
@@ -59,3 +59,162 @@ func NewLedgerStore() ILedgerStore {
 	return cs
 }
 
+func NewChainStore(file string) (*ChainStore, error) {
+
+	return &ChainStore{
+		st:                 NewStore(),
+		headerIndex:        map[uint32]Uint256{},
+		blockCache:         map[Uint256]*Block{},
+		headerCache:        map[Uint256]*Header{},
+		currentBlockHeight: 0,
+		storedHeaderCount:  0,
+		disposed:           false,
+	}, nil
+}
+
+func (bd *ChainStore) InitLedgerStoreWithGenesisBlock(genesisBlock *Block, defaultBookKeeper []*crypto.PubKey) (uint32, error) {
+
+	hash := genesisBlock.Hash()
+	bd.headerIndex[0] = hash
+	log.Debug(fmt.Sprintf("listhash genesis: %x\n", hash))
+
+	prefix := []byte{byte(IX_Version)}
+	version, err := bd.st.Get(prefix)
+	if err != nil {
+		version = []byte{0x00}
+	}
+
+	if version[0] == 0x01 {
+		currentBlockPrefix := []byte{byte(SYS_CurrentBlock)}
+		data, err := bd.st.Get(currentBlockPrefix)
+		if err != nil {
+			return 0, err
+		}
+
+		r := bytes.NewReader(data)
+		var blockHash Uint256
+		blockHash.Deserialize(r)
+		bd.currentBlockHeight, err = serialization.ReadUint32(r)
+		current_Header_Height := bd.currentBlockHeight
+
+
+		var headerHash Uint256
+		currentHeaderPrefix := []byte{byte(SYS_CurrentHeader)}
+		data, err = bd.st.Get(currentHeaderPrefix)
+		if err == nil {
+			r = bytes.NewReader(data)
+			headerHash.Deserialize(r)
+
+			headerHeight, err_get := serialization.ReadUint32(r)
+			if err_get != nil {
+				return 0, err_get
+			}
+
+			current_Header_Height = headerHeight
+		}
+
+		log.Debug(fmt.Sprintf("blockHash: %x\n", blockHash.ToArray()))
+		log.Debug(fmt.Sprintf("blockheight: %d\n", current_Header_Height))
+
+		var listHash Uint256
+		iter := bd.st.NewIterator([]byte{byte(IX_HeaderHashList)})
+		for iter.Next() {
+			rk := bytes.NewReader(iter.Key())
+			_, _ = serialization.ReadBytes(rk, 1)
+			startNum, err := serialization.ReadUint32(rk)
+			if err != nil {
+				return 0, err
+			}
+			log.Debug(fmt.Sprintf("start index: %d\n", startNum))
+
+			r = bytes.NewReader(iter.Value())
+			listNum, err := serialization.ReadVarUint(r, 0)
+			if err != nil {
+				return 0, err
+			}
+
+			for i := 0; i < int(listNum); i++ {
+				listHash.Deserialize(r)
+				bd.headerIndex[startNum+uint32(i)] = listHash
+				bd.storedHeaderCount++
+			}
+		}
+
+		if bd.storedHeaderCount == 0 {
+			iter = bd.st.NewIterator([]byte{byte(DATA_BlockHash)})
+			for iter.Next() {
+				rk := bytes.NewReader(iter.Key())				_, _ = serialization.ReadBytes(rk, 1)
+				listheight, err := serialization.ReadUint32(rk)
+				if err != nil {
+					return 0, err
+				}
+
+				r := bytes.NewReader(iter.Value())
+				listHash.Deserialize(r)
+
+
+				bd.headerIndex[listheight] = listHash
+			}
+		} else if current_Header_Height >= bd.storedHeaderCount {
+			hash = headerHash
+			for {
+				if hash == bd.headerIndex[bd.storedHeaderCount-1] {
+					break
+				}
+
+				header, err := bd.GetHeader(hash)
+				if err != nil {
+					return 0, err
+				}
+
+
+				bd.headerIndex[header.Blockdata.Height] = hash
+				hash = header.Blockdata.PrevBlockHash
+			}
+		}
+
+		return current_Header_Height, nil
+
+	} else {
+
+		bd.st.NewBatch()
+		iter := bd.st.NewIterator(nil)
+		for iter.Next() {
+			bd.st.BatchDelete(iter.Key())
+		}
+		iter.Release()
+
+		err := bd.st.BatchCommit()
+		if err != nil {
+			return 0, err
+		}
+
+		sort.Sort(crypto.PubKeySlice(defaultBookKeeper))
+
+
+		bkListKey := bytes.NewBuffer(nil)
+		bkListKey.WriteByte(byte(SYS_CurrentBookKeeper))
+
+
+		bkListValue := bytes.NewBuffer(nil)
+		serialization.WriteUint8(bkListValue, uint8(len(defaultBookKeeper)))
+		for k := 0; k < len(defaultBookKeeper); k++ {
+			defaultBookKeeper[k].Serialize(bkListValue)
+		}
+
+
+		serialization.WriteUint8(bkListValue, uint8(len(defaultBookKeeper)))
+		for k := 0; k < len(defaultBookKeeper); k++ {
+			defaultBookKeeper[k].Serialize(bkListValue)
+		}
+
+		bd.persist(genesisBlock)
+		err = bd.st.Put(prefix, []byte{0x01})
+		if err != nil {
+			return 0, err
+		}
+
+		return 0, nil
+	}
+}
+
