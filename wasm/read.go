0f4568a6d5e9ccc83aaa6b1ef7385b1d05ab030b
MemoryIX <admin@x-block.io>
2018-11-26 20:17:13 +0800

    Update read.go

--- wasm/read.go
+++ wasm/read.go
@@ -8,3 +8,13 @@ import (
 	"github.com/go-interpreter/wagon/wasm/leb128"
 )
 
+func readBytes(r io.Reader, n int) ([]byte, error) {
+	bytes := make([]byte, n)
+	_, err := io.ReadFull(r, bytes)
+	if err != nil {
+		return bytes, err
+	}
+
+	return bytes, nil
+}
+
