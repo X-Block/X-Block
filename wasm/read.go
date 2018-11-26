5ed4c5e62af6e50700e49c499cbee2a45974c5fe
MemoryIX <admin@x-block.io>
2018-11-26 20:17:56 +0800

    Update read.go

--- wasm/read.go
+++ wasm/read.go
@@ -42,3 +42,20 @@ func readStringUint(r io.Reader) (string, error) {
 	return readString(r, int(n))
 }
 
+func readU32(r io.Reader) (uint32, error) {
+	var buf [4]byte
+	_, err := io.ReadFull(r, buf[:])
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint32(buf[:]), nil
+}
+
+func readU64(r io.Reader) (uint64, error) {
+	var buf [8]byte
+	_, err := io.ReadFull(r, buf[:])
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint64(buf[:]), nil
+}
