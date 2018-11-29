9f2f164fa5c6c9c0f3517dadd193d6346a6cbe8d
MemoryIX <admin@x-block.io>
2018-11-29 11:08:22 +0800

    Update module.go

--- vm/wasm/module.go
+++ vm/wasm/module.go
@@ -17,3 +17,40 @@ type Function struct {
 	Host reflect.Value
 }
 
+func (fct *Function) IsHost() bool {
+	return fct.Host != reflect.Value{}
+}
+
+type Module struct {
+	Version  uint32
+	Sections []Section
+
+	Types    *SectionTypes
+	Import   *SectionImports
+	Function *SectionFunctions
+	Table    *SectionTables
+	Memory   *SectionMemories
+	Global   *SectionGlobals
+	Export   *SectionExports
+	Start    *SectionStartFunction
+	Elements *SectionElements
+	Code     *SectionCode
+	Data     *SectionData
+	Customs  []*SectionCustom
+
+
+	FunctionIndexSpace []Function
+	GlobalIndexSpace   []GlobalEntry
+
+	TableIndexSpace        [][]uint32
+	LinearMemoryIndexSpace [][]byte
+
+	imports struct {
+		Funcs    []uint32
+		Globals  int
+		Tables   int
+		Memories int
+	}
+}
+
+
