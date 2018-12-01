c965e74c398d08e70b5ed2e3af8acd3cab6045dc
MemoryIX <admin@x-block.io>
2018-12-01 10:59:12 +0800

    Update moduleTest.go

--- vm/wasm/moduleTest.go
+++ vm/wasm/moduleTest.go
@@ -16,3 +16,29 @@ var testPaths = []string{
 	"../exec/testdata/spec",
 }
 
+func TestReadModule(t *testing.T) {
+	for _, dir := range testPaths {
+		fnames, err := filepath.Glob(filepath.Join(dir, "*.wasm"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, fname := range fnames {
+			name := fname
+			t.Run(filepath.Base(name), func(t *testing.T) {
+				raw, err := ioutil.ReadFile(name)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				r := bytes.NewReader(raw)
+				m, err := wasm.ReadModule(r, nil)
+				if err != nil {
+					t.Fatalf("error reading module %v", err)
+				}
+				if m == nil {
+					t.Fatalf("error reading module: (nil *Module)")
+				}
+			})
+		}
+	}
+}
