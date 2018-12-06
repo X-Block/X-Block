11e2ba8fe1db043fe8545a3774434c77487ecb98
X-Block <Bomou2018>
2018-12-06 20:08:34 +0800

    Update stack.go

--- stack/stack.go
+++ stack/stack.go
@@ -4,3 +4,13 @@ type Stack struct {
 	slice []uint64
 }
 
+func (s *Stack) Push(b uint64) {
+	s.slice = append(s.slice, b)
+}
+
+func (s *Stack) Pop() uint64 {
+	v := s.Top()
+	s.slice = s.slice[:len(s.slice)-1]
+	return v
+}
+
