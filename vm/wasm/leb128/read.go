803170f8e9bed64fa0e4abe503bfbbfbbfdfebbd
MemoryIX <admin@x-block.io>
2018-12-06 20:13:13 +0800

    Update read.go

--- vm/wasm/leb128/read.go
+++ vm/wasm/leb128/read.go
@@ -39,3 +39,33 @@ func ReadVarint32(r io.Reader) (int32, error) {
 	return n, err
 }
 
+func ReadVarint64Size(r io.Reader) (res int64, size uint, err error) {
+	var shift uint
+	var sign int64 = -1
+	b := make([]byte, 1)
+
+	for {
+		if _, err = io.ReadFull(r, b); err != nil {
+			return
+		}
+		size++
+
+		cur := int64(b[0])
+		res |= (cur & 0x7f) << shift
+		shift += 7
+		sign <<= 7
+		if cur&0x80 == 0 {
+			break
+		}
+	}
+
+	if ((sign >> 1) & res) != 0 {
+		res |= sign
+	}
+	return res, size, nil
+}
+
+func ReadVarint64(r io.Reader) (int64, error) {
+	n, _, err := ReadVarint64Size(r)
+	return n, err
+}
