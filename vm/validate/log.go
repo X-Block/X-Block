a1c959a63d5ce671dd16a29a8b478894d82d6a9c
DocXBlock <info@x-block.io>
2018-12-19 10:43:55 +0800

    Create log.go

---
+++ vm/validate/log.go
@@ -0,0 +1,23 @@
+
+package validate
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var PrintDebugInfo = false
+
+var logger *log.Logger
+
+func init() {
+	w := ioutil.Discard
+
+	if PrintDebugInfo {
+		w = os.Stderr
+	}
+
+	logger = log.New(w, "", log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
+}
