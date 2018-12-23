d63f9a79c025e062b8a54e4c27114413fffc0f2b
MemoryIX <admin@x-block.io>
2018-12-23 09:33:57 +0800

    Update vm.go

--- vm/validate/vm.go
+++ vm/validate/vm.go
@@ -20,3 +20,63 @@ type mockVM struct {
 	curFunc *wasm.FunctionSig
 }
 
+type block struct {
+	pc          int            
+	stackTop    int            
+	blockType   wasm.BlockType 
+	op          byte           
+	polymorphic bool           
+	loop        bool           
+}
+
+func (vm *mockVM) fetchVarUint() (uint32, error) {
+	return leb128.ReadVarUint32(vm.code)
+}
+
+func (vm *mockVM) fetchVarInt() (int32, error) {
+	return leb128.ReadVarint32(vm.code)
+}
+
+func (vm *mockVM) fetchVarInt64() (int64, error) {
+	return leb128.ReadVarint64(vm.code)
+}
+
+func (vm *mockVM) fetchUint32() (uint32, error) {
+	var buf [4]byte
+	_, err := io.ReadFull(vm.code, buf[:])
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint32(buf[:]), nil
+}
+
+func (vm *mockVM) fetchUint64() (uint64, error) {
+	var buf [8]byte
+	_, err := io.ReadFull(vm.code, buf[:])
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint64(buf[:]), nil
+}
+
+func (vm *mockVM) pushBlock(op byte, blockType wasm.BlockType) {
+	logger.Printf("Pushing block %v", blockType)
+	vm.blocks = append(vm.blocks, block{
+		pc:          vm.pc(),
+		stackTop:    vm.stackTop,
+		blockType:   blockType,
+		polymorphic: vm.isPolymorphic(),
+		op:          op,
+		loop:        op == ops.Loop,
+	})
+}
+
+
+func (vm *mockVM) getBlockFromDepth(depth int) *block {
+	if depth >= len(vm.blocks) {
+		return nil
+	}
+
+	return &vm.blocks[len(vm.blocks)-1-depth]
+}
+
