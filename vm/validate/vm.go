eeddf0a71e468228ea84fae1e38451e9ad4177eb
MemoryIX <admin@x-block.io>
2018-12-23 09:34:17 +0800

    Update vm.go

--- vm/validate/vm.go
+++ vm/validate/vm.go
@@ -80,3 +80,101 @@ func (vm *mockVM) getBlockFromDepth(depth int) *block {
 	return &vm.blocks[len(vm.blocks)-1-depth]
 }
 
+func (vm *mockVM) canBranch(depth int) error {
+	blockType := wasm.BlockTypeEmpty
+
+	block := vm.getBlockFromDepth(depth)
+	if block == nil {
+		if depth == len(vm.blocks) {
+			if len(vm.curFunc.ReturnTypes) != 0 {
+				blockType = wasm.BlockType(vm.curFunc.ReturnTypes[0])
+			}
+		} else {
+			return InvalidLabelError(uint32(depth))
+		}
+	} else if !block.loop {
+		blockType = block.blockType
+	}
+
+	if blockType != wasm.BlockTypeEmpty {
+		top, under := vm.topOperand()
+		if under || top.Type != wasm.ValueType(blockType) {
+			return InvalidTypeError{wasm.ValueType(blockType), top.Type}
+		}
+	}
+
+	return nil
+}
+
+
+func (vm *mockVM) popBlock() *block {
+	if len(vm.blocks) == 0 {
+		return nil
+	}
+
+	stackTop := len(vm.blocks) - 1
+	block := vm.blocks[stackTop]
+	vm.blocks = append(vm.blocks[:stackTop], vm.blocks[stackTop+1:]...)
+
+	return &block
+}
+
+func (vm *mockVM) topBlock() *block {
+	if len(vm.blocks) == 0 {
+		return nil
+	}
+
+	return &vm.blocks[len(vm.blocks)-1]
+}
+
+func (vm *mockVM) topOperand() (o operand, under bool) {
+	stackTop := vm.stackTop - 1
+	if stackTop == -1 {
+		under = true
+		return
+	}
+	o = vm.stack[stackTop]
+	return
+}
+
+func (vm *mockVM) popOperand() (operand, bool) {
+	var o operand
+	stackTop := vm.stackTop - 1
+	if stackTop == -1 {
+		return o, true
+	}
+	o = vm.stack[stackTop]
+	vm.stackTop--
+
+	logger.Printf("Stack after pop is %v. Popped %v", vm.stack[:vm.stackTop], o)
+	return o, false
+}
+
+func (vm *mockVM) pushOperand(t wasm.ValueType) {
+	o := operand{t}
+	logger.Printf("Stack top: %d, Len of stack :%d", vm.stackTop, len(vm.stack))
+	if vm.stackTop == len(vm.stack) {
+		vm.stack = append(vm.stack, o)
+	} else {
+		vm.stack[vm.stackTop] = o
+	}
+	vm.stackTop++
+
+	logger.Printf("Stack after push is %v. Pushed %v", vm.stack[:vm.stackTop], o)
+}
+
+func (vm *mockVM) adjustStack(op ops.Op) error {
+	for _, t := range op.Args {
+		op, under := vm.popOperand()
+		if !vm.isPolymorphic() && (under || op.Type != t) {
+			return InvalidTypeError{t, op.Type}
+		}
+	}
+
+	if op.Returns != wasm.ValueType(wasm.BlockTypeEmpty) {
+		vm.pushOperand(op.Returns)
+	}
+
+	return nil
+}
+
