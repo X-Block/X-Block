78120323f778ee9e273998578d8bc66b3642f9f7
MemoryIX <admin@x-block.io>
2018-12-27 10:42:45 +0800

    Update hex.go

--- vm/wasm/hex.go
+++ vm/wasm/hex.go
@@ -8,3 +8,19 @@ import (
 )
 
 
+func hexDump(data []byte, offset uint) string {
+	buf := new(bytes.Buffer)
+	d := &dumper{w: buf, n: offset}
+	d.Write(data)
+	d.Close()
+	return buf.String()
+}
+
+type dumper struct {
+	w          io.Writer
+	rightChars [18]byte
+	buf        [14]byte
+	used       int  
+	n          uint 
+}
+
